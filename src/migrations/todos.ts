fa6cc8636b72b9c90fbc0601afbd7aeeec879674
hovocache <hovhannes.shakhulyan@gmail.com>
2025-09-27 20:02:14 +0400

    Todos new props with migrations

--- src/migrations/todos.ts
+++ src/migrations/todos.ts
@@ -1,11 +1,30 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 
 async function run() {
-  await mongoose.connect(`mongodb://localhost:27017/todosDB`);
-  await mongoose.connection.collection('todos').updateMany(
-    { status: { $exists: false } },
-    { $set: { status: 'new' } }
-  );
+  await mongoose.connect(`${process.env.MONGO_DB_URI}/${process.env.MONGO_DB_NAME}`);
+
+await mongoose.connection.collection('todos').updateMany(
+  {
+    $or: [
+      { status: { $exists: false } },
+      { initiator: { $exists: false } },
+      { performer: { $exists: false } },
+      { reviewers: { $exists: false } },
+    ]
+  },
+  {
+    $set: {
+      status: 'new',
+      initiator: null,
+      performer: null,
+      reviewers: [],  
+    }
+  }
+);
 
   console.log('Todos migration complete');
 
